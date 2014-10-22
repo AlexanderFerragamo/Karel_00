55e9217afd7bc5c6669a08db2e38788815324ed4
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-10-22 10:54:36 +0100

    Robot and questions

--- Lab00.java
+++ Lab00.java
@@ -14,9 +14,20 @@ public class Lab00 {
      public static void main(String[] args) {
              Display.openWorld("maps/first.map");
    Display.setSize(10, 10);
-   Display.setSpeed(3);
+   Display.setSpeed(1);
+   
    Robot karel = new Robot();
+   
    karel.move();
+   karel.pickBeeper();
    karel.move();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.turnLeft();
+   karel.turnLeft();
+   
+   
      }
 }
